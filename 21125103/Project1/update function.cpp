31b69dceeb82e75e118a30fa4203ca7e96f8bf59
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2022-03-15 20:23:42 +0700

    Hàm update : Không quan trọng

--- 21125103/Project1/update function.cpp
+++ 21125103/Project1/update function.cpp
@@ -1,19 +1,6 @@
+#include "Node.cpp"
 #include <iostream>
-using namespace std;
-struct HS
-{
-    char ID[10];
-};
-struct HK
-{
-    char ID[10];
-    char*teacher;
-    char time[6];
-    char date[13];
-    char*name;
-};
-void update(HK*&Hoc_ky);
-void update(HK*&Hoc_ky)
+void update(LinkedList<Semester*> pSemester)
 {
     int level;
     cout << "PRESS 1 TO UPDATE ID\nPRESS 2 TO UPDATE STUDENT LIST\nPRESS 3 TO CHANGE TEACHER\nPRESS 4 TO CHANGE TO SCHEDULE";
@@ -23,16 +10,39 @@ void update(HK*&Hoc_ky)
         case 1:
         {
             cout << "Which HP to change";
-            char*input;
-            
+            string ID;
+            cin >> ID;
+            while(pHead!= nullptr)
+            {
+                if (pHead->id == ID)
+                {
+                    cout << "Change to : ";
+                    cin >> ID;
+                    pHead->id = ID;
+                    update(pHead);
+                    break;
+                }
+            }
+            //Nhập Mã môn học vào, nếu loop qua linked list không tìm thấy coi như not found. Sẽ có 2 option : 1 là thêm, 2 là skip qua
+            cout << "Hoc Phan khong tim thay\n";//Bước skip
+            update(pHead);//Nhảy lại menu 
         }
         case 2:
         {
-
+            cout << "ID of student : ";
+            string input;
+            cin>> input;
+            
         }
         case 3 :
         {
-
+            cout << "Giao vien moi : ";
+            string name;
+            cin >> name;
+            while(pHead!=nullptr)
+            {
+                if (pHead->pCourse)
+            }
         }
         case 4 :
         {
