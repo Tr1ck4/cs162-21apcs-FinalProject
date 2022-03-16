40d916e89fc94602fde1f7baaa532430b2bc2e28
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2022-03-16 08:27:16 +0700

    Update của Phát

--- 21125103/Project1/Node.cpp
+++ 21125103/Project1/Node.cpp
@@ -16,14 +16,126 @@ SchoolYear
 
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
+// Node class
+template <typename T>
+struct Node {
+    T data;
+    Node<T>* pNext;
 
-template <typename T> struct LinkedList {
-    T a;
-    /// chuwa code :(
+    Node() : pNext(nullptr) {}
+    Node(const T& item, Node<T>* ptr = nullptr) :
+        data(item), pNext(ptr) {}
 };
+
+// Linked list class
+template <typename T>
+struct LinkedList {
+    Node<T>* pHead;
+    Node<T>* pTail;
+
+    LinkedList() {
+        // Create the dummy head node
+        pHead = pTail = new Node<T>();
+    }
+    ~LinkedList() {
+        while (pHead->pNext != nullptr) {
+            Node<T>* temp = pHead;
+            pHead = pHead->pNext;
+            delete temp;
+        }
+        delete pHead;
+    }
+
+    // class iterator
+    struct iterator {
+        Node<T>* nodePtr;
+        iterator(Node<T> *newPtr) : nodePtr(newPtr) {}
+        iterator() : nodePtr(nullptr) {}
+
+        bool operator!=(const iterator& itr) const {
+            return nodePtr != itr.nodePtr;
+        }
+
+        T& operator*() const {
+            return nodePtr->pNext->data;
+        }
+
+        iterator operator++(int) {
+            nodePtr = nodePtr->pNext;
+            return *this;
+        }
+
+        iterator operator++() {
+            nodePtr = nodePtr->pNext;
+            return *this;
+        }
+    };
+
+    iterator begin() const {
+        return iterator(pHead);
+    }
+
+    iterator end() const {
+        return iterator(pTail);
+    }
+
+    iterator insert(iterator position, const T& value) {
+        Node<T>* newNode = new Node<T>(value, position.nodePtr->pNext);
+        if (position.nodePtr == pTail) {
+            pTail = newNode;
+        }
+        position.nodePtr->pNext = newNode;
+        return position;
+    }
+
+    iterator push_back(const T& value) {
+        return insert(end(), value);
+    }
+
+    iterator erase(iterator position) {
+        Node<T> *toDelete = position.nodePtr->pNext;
+        position.nodePtr->pNext = position.nodePtr->pNext->pNext;
+        if (toDelete == pTail) {
+            pTail = position.nodePtr;
+        }
+        delete toDelete;
+        return position;
+    }
+
+    iterator find(const T& value) {
+        iterator cur(pHead);
+        while (cur != end()) {
+            if (*cur == value) {
+                return cur;
+            }
+            ++cur;
+        }
+        return cur;
+    }
+};
+
+
+int main() {
+    LinkedList<int> v;
+
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+
+    for (auto x : v) {
+        cout << x << ' ' << endl;
+    }
+
+    if (v.find(5) != v.end()) {
+        cout << *v.find(5) << '\n';
+    }
+
+    return 0;
+}
 
 struct Date;
 struct Student;
@@ -82,7 +194,3 @@ struct User {
 // co them 1 file list het ID ra de doc luc dau cho de. 
 /// doc du lieu: doc id truoc roi moi doc cac thong tin khac
 /// luu du lieu ngay sau moi lan thay doi
-
-int main() {
-    /// Sad :<
-}
